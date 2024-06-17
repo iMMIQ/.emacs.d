f78a1a1ad0b5ec1f1a5298a3e8eb4c7f7397aa12
iMMIQ <ayd@immiq.top>
2024-06-18 00:37:29 +0800

    python mode enhance

--- config/python.el
+++ config/python.el
@@ -1,8 +1,21 @@
-(add-hook 'python-mode-hook
-          (lambda ()
-            (setq lsp-bridge-python-command "python3")
-            (setq lsp-bridge-python-lsp-server "pylsp")
-            (setq lsp-bridge-enable-hover-diagnostic t)
-            (lsp-bridge-mode)
-            ;; Configure 'black' formatter for 'apheleia'
-            (setf (alist-get 'black apheleia-formatters) '("black" "-S" "-l" "120" "-"))))
+(use-package python
+  :mode ("[./]flake8\\'" . conf-mode)
+  :mode ("/Pipfile\\'" . conf-mode)
+  :init
+  (setq python-shell-interpreter "python3")
+  (setq python-indent-guess-indent-offset-verbose nil)
+
+  ;; lsp-bridge
+  (setq lsp-bridge-python-command "python3")
+  (setq lsp-bridge-python-lsp-server
+	(cond
+	 ((executable-find "pylsp") "pylsp")
+	 ((executable-find "Microsoft.Python.LanguageServer") "Microsoft.Python.LanguageServer")
+	 (t "pyright")))
+  (setq lsp-bridge-enable-hover-diagnostic t)
+
+  ;; Configure 'black' formatter for 'apheleia'
+  (setf (alist-get 'black apheleia-formatters) '("black" "-S" "-l" "120" "-"))
+
+  :config
+  (lsp-bridge-mode))
