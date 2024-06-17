ea5be3bd0ff4f763de083311b0323287c25451b7
iMMIQ <ayd@immiq.top>
2024-06-18 01:02:44 +0800

    python enhance
    formatters
    import check
    isort

--- config/python.el
+++ config/python.el
@@ -1,4 +1,6 @@
 (use-package python
+  :straight t
+  :defer t
   :mode ("[./]flake8\\'" . conf-mode)
   :mode ("/Pipfile\\'" . conf-mode)
   :init
@@ -14,8 +16,29 @@
 	 (t "pyright")))
   (setq lsp-bridge-enable-hover-diagnostic t)
 
-  ;; Configure 'black' formatter for 'apheleia'
-  (setf (alist-get 'black apheleia-formatters) '("black" "-S" "-l" "120" "-"))
+  (dolist (formatter
+           '(("black" . ("black" "-S" "-l" "120" "-"))
+             ("yapf" . ("yapf" "--style" "{based_on_style: pep8, column_limit: 120}" "-"))
+             ("autopep8" . ("autopep8" "--max-line-length" "120" "-"))))
+    (when (executable-find (car formatter))
+      (setf (alist-get (intern (car formatter)) apheleia-formatters) (cdr formatter))))
 
   :config
   (lsp-bridge-mode))
+
+(let ((pyflakes-path (executable-find "pyflakes")))
+  (when pyflakes-path
+    (use-package pyimport
+      :straight t
+      :defer t
+      :hook (python-mode . pyimport-mode)
+      :config
+      (setq pyimport-pyflakes-path pyflakes-path))))
+
+(when (executable-find "isort")
+  (use-package py-isort
+    :straight t
+    :defer t
+    :hook (before-save . py-isort-before-save)
+    :config
+    (setq py-isort-options '("-l 120" "--profile=black"))))
