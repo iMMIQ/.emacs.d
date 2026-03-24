7af457f3165d5a37337bb8ce112ef45d185d4d32
immiq <ayd@immiq.top>
2026-03-24 14:17:51 +0800

    fix: narrow optional tree module

--- lisp/tools/tree.el
+++ lisp/tools/tree.el
@@ -4,31 +4,6 @@
 
 ;;; Code:
 
-(defconst tools-tree--config-root
-  (or (let ((origin (or load-file-name
-                        byte-compile-current-file
-                        buffer-file-name
-                        default-directory)))
-        (and origin
-             (locate-dominating-file
-              origin
-              "straight/repos/straight.el/bootstrap.el")))
-      user-emacs-directory)
-  "Root directory for this config checkout.")
-
-(defconst tools-tree--straight-bootstrap-file
-  (expand-file-name "straight/repos/straight.el/bootstrap.el"
-                    tools-tree--config-root)
-  "Path to the local straight.el bootstrap file.")
-
-(defun tools-tree--bootstrap-straight ()
-  "Enable local straight/use-package integration when available."
-  (when (file-exists-p tools-tree--straight-bootstrap-file)
-    (load tools-tree--straight-bootstrap-file nil 'nomessage))
-  (require 'use-package))
-
-(tools-tree--bootstrap-straight)
-
 (use-package treemacs
   :straight t
   :commands (treemacs treemacs-select-window))
